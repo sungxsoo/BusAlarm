2375225fd8d92e93d42553614e35abdc3480950a
sungxsoo <njs783075@gmail.com>
2021-10-06 00:04:13 +0900

    time logic version 2

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,8 @@
-import java.text.ParseException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-class DateFormat {
-	
-	int totalMin;
-	
-	public DateFormat(String date){
-		
-		String[] time = date.split(":");
-		//8:00이라
-		//time[0] 은 8,time[1]은 00 
-		totalMin = Integer.parseInt(time[0])*60 + Integer.parseInt(time[1]);
 
-	}
-	public int getTime() {
-		return totalMin;
-	}
-}
 
 
 public class Main {
@@ -25,43 +10,53 @@ public class Main {
 	public static String[] stationNames = {"버스관리사무소", "상공회의소", "진입로", "동부경찰서", "용인시장", "중앙주차장"};
 	public static int[] stationTimes = {3, 2, 1, 3, 4, 2};
 	public static DateFormat[]stationStart;
-	
-	
-	//현재 시간과 매칭되는 버스 시간대 구하기
-	public static void getCurtrentBusTime(String stationName) {
-	
-		int index;
-		//임시 버스 시작 데이터 
-		stationStart[0] = new DateFormat("8:00");
-		stationStart[1] = new DateFormat("8:10");
-		stationStart[2] = new DateFormat("9:05");
-		stationStart[3] = new DateFormat("10:30");
-		stationStart[4] = new DateFormat("11:20");
-		stationStart[5] = new DateFormat("13:10");
-		stationStart[6] = new DateFormat("14:50");
-		stationStart[7] = new DateFormat("15:50");
-		stationStart[8] = new DateFormat("16:00");
-		stationStart[9] = new DateFormat("16:30");
-		stationStart[10] = new DateFormat("16:50");
-		
-		//매칭되는 정류장의 이름의 index를 구한다. 
+
+
+	public static int getCurrentBusTime() {
+		int index = 0;
+
+		long now = System.currentTimeMillis();
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+		Date date = new Date(now);
+
+		//현재시간을 비교 포멧으로 변환
+		DateFormat targetTime = new DateFormat(format.format(date));
+
+		//현재 시간과 가장 가까운 시간대 버스 구하기
+		while(stationStart[index].getTotalMin() <= targetTime.getTotalMin()&&index<stationStart.length-1){
+			index++;
+		}
+
+
+		return stationStart[index].getTotalMin();
+	}
+
+	// 현재시간에서 가장 가까운 출발대의 버스시간에 파라메터로 주어진 정류장부터 종착지까지 예상 시간을 반환한다.
+	public static void getArrivalTime(String stationName) {
+
+		int stationIndex = 0;
+		int arrivalTime = 0;
+
+		//매칭되는 정류장의 이름의 index를 구한다.
 		for(int i =0; i<stationNames.length; i++) {
 			if(stationNames[i].equals(stationName)) {
-				index = i;
+				stationIndex = i;
 			}
 		}
-		
-		//현재 시간 구하기
-		long now = System.currentTimeMillis();
-		SimpleDateFormat format = new SimpleDateFormat("hh:mm");
-		Date date = new Date(now);
-		System.out.println(format.format(date));
-		
-		//현재 시간과 가장 가까운 시간대 버스 구하기 
-		
-		
-		
-		
-		
+
+		//도착 시간 = 버스 출발 시간 + 현재 정류장 부터 도착지까지 걸리는 시간
+		arrivalTime = getCurrentBusTime();
+		for(int i =stationIndex+1; i<stationTimes.length; i++) {
+			arrivalTime =+ stationTimes[i];
+		}
+
+	}
+	public static void main(String[] args) throws IOException {
+		CSVReader reader = new CSVReader();
+		reader.read(".//csv//명지대역.csv");
+
+		stationStart = reader.getLinesInFormat();
+
+		System.out.println(getCurrentBusTime());
 	}
 }
